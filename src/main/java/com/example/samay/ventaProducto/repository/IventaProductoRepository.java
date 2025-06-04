42844cf647e6eff107497b12f9c362e1b3fff721
VeronicaCardenasR <crveronica20@gmail.com>
2025-06-04 12:51:07 -0500

    cambios venta-producto

--- src/main/java/com/example/samay/ventaProducto/repository/IventaProductoRepository.java
+++ src/main/java/com/example/samay/ventaProducto/repository/IventaProductoRepository.java
@@ -4,7 +4,10 @@ import com.example.samay.ventaProducto.model.VentaProducto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IventaProductoRepository extends JpaRepository<VentaProducto, Long> {
+    List<VentaProducto> findByVenta_VentaId(Long ventaId);
 
 }
