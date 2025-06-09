f4ea269658a4202c1b741362f1d055525c1d6714
VeronicaCardenasR <crveronica20@gmail.com>
2025-06-09 12:13:59 -0500

    cros

--- src/main/java/com/example/samay/CorsConfig.java
+++ src/main/java/com/example/samay/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5500")
+                        .allowedOrigins("http://127.0.0.1:5500", "https://main.d3nlxcdq9iz53.amplifyapp.com/")
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
