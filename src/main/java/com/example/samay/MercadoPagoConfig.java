dcf24b744f3d70a148ab7cc63ebe60371289c8f3
Jorge Ramirez <46687749+jorgeramirez2018@users.noreply.github.com>
2025-06-09 16:59:26 -0500

    Add files via upload

---
+++ src/main/java/com/example/samay/MercadoPagoConfig.java
@@ -0,0 +1,15 @@
+package com.example.samay;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+public class MercadoPagoConfig {
+
+    @PostConstruct
+    public void init() {
+        com.mercadopago.MercadoPagoConfig.setAccessToken("TEST-842565025555115-052521-61af028e01929d96e60ed748860348e4-726255972");
+    }
+}
